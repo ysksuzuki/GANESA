be9a43de25914e5605cebfd38c91b206dc819035
Yusuke Suzuki <musician@voice.ocn.ne.jp>
2017-03-24 19:53:48 +0900

    Add unit test case

--- src/main/scala/ganesa/util/NumberUtil.scala
+++ src/main/scala/ganesa/util/NumberUtil.scala
@@ -5,6 +5,7 @@ import scala.math.BigDecimal.RoundingMode
 
 object NumberUtil {
   def inchToCm(inch: Int, scale: Int = 100) = {
+    require(inch >= 0)
     val base = "2.54"
     (BigDecimal(inch) / BigDecimal(scale) * BigDecimal(base)).setScale(1, RoundingMode.HALF_UP)
   }
